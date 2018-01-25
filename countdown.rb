06a588c13adbd53c545ade392e9d4821ea60be97
Veronica Bustabad <vbustabad@gmail.com>
2018-01-25 23:27:25 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 number = 10
 
 def countdown(number)
-    while number > 0
-    number -= number
-    puts "#{number} SECOND(S)!"
+  while number > 0
+  number -= number
+  puts "#{number} SECOND(S)!"
   end
 end
